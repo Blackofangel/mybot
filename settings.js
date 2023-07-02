eee12bb3f2757a624f3e6b85b75156d810e8891e
Blackofangel <114835954+Blackofangel@users.noreply.github.com>
2023-07-02 18:03:54 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,21 +21,20 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['916906137213'] //ur owner number
+global.ownernomer = "916906137213" //ur owner number2
+global.ownername = "Blackofangel" //ur owner name
+global.ytname = "YT: godn2841" //ur yt chanel name
+global.socialm = "GitHub: Blackofangel" //ur github or insta name
+global.location = "Italy, Trevi, Collebasso" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "Il salotto infinito"
+global.ownernumber = '3277180668'
+global.ownername = 'Blackofangel'
+global.ownerNumber = ["3277180668@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@godn2841"
+global.wagc = "https://chat.whatsapp.com/BnGhbI5hMZiEtgjOPJJ0YG"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
